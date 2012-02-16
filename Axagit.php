107634807dde10c2d7dfe069b039c7f09bbc1f45
Amal Bose <axatrikx@gmail.com>
2012-02-16 21:00:39 +0530

    Corrected cached file support

--- Axagit.php
+++ Axagit.php
@@ -1,33 +1,39 @@
 <?php
-include_once("Project.php");
-$CACHE_FILE_NAME="cache.json";
-function getAllProjects(){
+include_once ("Project.php");
+$CACHE_FILE_NAME = "cache.json";
+function getAllProjects() {
 
-	$ProjectArray = array();
+	$ProjectArray = array ();
 	$jsonString = get_content_from_github('http://github.com/api/v2/json/repos/show/Axatrikx/');
-	$jsonResults =  json_decode($jsonString,true);
+	$jsonResults = json_decode($jsonString, true);
 
-	$repositories=$jsonResults['repositories'];
+	$repositories = $jsonResults['repositories'];
 	$noOfRepositories = sizeof($repositories);
-	if($noOfRepositories>0){
+	if ($noOfRepositories > 0) {
 		foreach ($repositories as $key) {
-			$ProjectArray[]=getProjectObject($key);
-			$fp = fopen('cache.json', 'w');
-			fwrite($fp, $jsonString);
-			fclose($fp);
+			$ProjectArray[] = getProjectObject($key);
 		}
+		$fp = fopen('cache.json', 'w');
+		fwrite($fp, $jsonString);
+		fclose($fp);
+	} else {
+		try {
+			$line="";
+			$file_handle = fopen("cache.json", "r");
+			while (!feof($file_handle)) {
+				$line.= fgets($file_handle);
+			}
+			fclose($file_handle);
+			$jsonResults1 = json_decode($line, true);
+			$repositories = $jsonResults1['repositories'];
+			$noOfRepositories = sizeof($repositories);
+			foreach ($repositories as $key) {
+				$ProjectArray[] = getProjectObject($key);
+			}
+		} catch (Exception $e) {
+		}
+
 	}
-	else {
-		try{
-			$string = file_get_contents("cache.json");
-			$ProjectArray=json_decode($string,true);
-		}
-		catch(Exception $e){
-			$ProjectArray="";
-		}
-		
-	}
-		
 	return $ProjectArray;
 
 }
@@ -35,19 +41,18 @@ function getAllProjects(){
 /* gets url */
 function get_content_from_github($url) {
 	$ch = curl_init();
-	curl_setopt($ch,CURLOPT_URL,$url);
-	curl_setopt($ch,CURLOPT_RETURNTRANSFER,1); 
-	curl_setopt($ch,CURLOPT_CONNECTTIMEOUT,1);
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 1);
 	$content = curl_exec($ch);
 	curl_close($ch);
 	return $content;
 }
 
-function getProjectObject($JSONProjectDetails){
+function getProjectObject($JSONProjectDetails) {
 	$project = new Project();
 	$project->setProjectDetails($JSONProjectDetails);
 	return $project;
 }
-
 ?>
 
--- index.php
+++ index.php
@@ -13,116 +13,123 @@
 <BR>
 Welcome to Axatrikx Projects
 <BR>
-<div id='projectList'>
+
 <?php
-include_once("Axagit.php");
+include_once ("Axagit.php");
 error_reporting(0);
 $projects = getAllProjects();
-?>
+?><div id='projectList'>
 	<div class='proheader narrowcurve'>
 	My Projects</div>
 	<div class='projecttype narrowcurve'>
 <?php
+
 foreach ($projects as $project) {
 
-	if(!$project->getForkType()){
-	?> 
+	if (!$project->getForkType()) {
+?> 
 
 	<div class="projects narrowcurve" >
-	<?php 
-	echo "<div class='proname narrowcurve'>";
-	echo "<div class='prourl'><a href='";
-	echo $project->getProjectURL();
-	echo "' >";
-	echo strtoupper($project->getProjectName());
-	echo "</a></div>";
-	echo "<div class='prolang'>";
-	echo $project->getProjectLanguage();
-	echo "</div>";
-	echo "</div>";
-	echo "<div class='prodesc'>";
-	echo $project->getProjectDescription();
-	echo "</div>";
-	echo "<div class='procont'>";
-	echo "<div class='createD'>";
-	echo $project->getProjectCreatedDate();
-	echo "</div>";
-	echo "<div class='homeURL'>";
-	echo "<a href='";
-	echo $project->getProjectHomeURL();
-	echo "'  class='button white' >Home URL</a>";
-	echo "</div>";
-	echo "</div>";
-	echo "<div class='procont'>";
-	echo "<div class='postD'>";
-	echo $project->getProjectPushDate();
-	echo "</div>";
-	echo "<div class='URL'>";
-	echo "<a href='";
-	echo $project->getProjectURL();
-	echo "'  class='button white'>URL</a>";
-	echo "</div>";
-	echo "</div>";
-	
-	?>
+	<?php
+
+		echo "<div class='proname narrowcurve'>";
+		echo "<div class='prourl'><a href='";
+		echo $project->getProjectURL();
+		echo "' >";
+		echo strtoupper($project->getProjectName());
+		echo "</a></div>";
+		echo "<div class='prolang'>";
+		echo $project->getProjectLanguage();
+		echo "</div>";
+		echo "</div>";
+		echo "<div class='prodesc'>";
+		echo $project->getProjectDescription();
+		echo "</div>";
+		echo "<div class='procont'>";
+		echo "<div class='createD'>";
+		echo $project->getProjectCreatedDate();
+		echo "</div>";
+		echo "<div class='homeURL'>";
+		echo "<a href='";
+		echo $project->getProjectHomeURL();
+		echo "'  class='button white' >Home URL</a>";
+		echo "</div>";
+		echo "</div>";
+		echo "<div class='procont'>";
+		echo "<div class='postD'>";
+		echo $project->getProjectPushDate();
+		echo "</div>";
+		echo "<div class='URL'>";
+		echo "<a href='";
+		echo $project->getProjectURL();
+		echo "'  class='button white'>URL</a>";
+		echo "</div>";
+		echo "</div>";
+?>
 	</div> <!-- projects -->
 	
 	<?php
+
 	}
-	
+
 }
 ?></div> <!--myprojects--><?php
+
 ?>
 	<div class='proheader narrowcurve'>
 	Forked Projects</div>
 	<div class='projecttype narrowcurve'>
 <?php
+
 foreach ($projects as $project) {
 
-	if($project->getForkType()){
-	?> 
+	if ($project->getForkType()) {
+?> 
 	<div class="projects narrowcurve" >
-	<?php 
-	echo "<div class='proname narrowcurve'>";
-	echo "<div class='prourl'><a href='";
-	echo $project->getProjectURL();
-	echo "' >";
-	echo strtoupper($project->getProjectName());
-	echo "</a></div>";
-	echo "<div class='prolang'>";
-	echo $project->getProjectLanguage();
-	echo "</div>";
-	echo "</div>";
-	echo "<div class='prodesc'>";
-	echo $project->getProjectDescription();
-	echo "</div>";
-	echo "<div class='procont'>";
-	echo "<div class='createD'>";
-	echo $project->getProjectCreatedDate();
-	echo "</div>";
-	echo "<div class='homeURL'>";
-	echo "<a href='";
-	echo $project->getProjectHomeURL();
-	echo "'  class='button white'>Home URL</a>";
-	echo "</div>";
-	echo "</div>";
-	echo "<div class='procont'>";
-	echo "<div class='postD'>";
-	echo $project->getProjectPushDate();
-	echo "</div>";
-	echo "<div class='URL '>";
-	echo "<a href='";
-	echo $project->getProjectURL();
-	echo "' class='button white' >URL</a>";
-	echo "</div>";
-	echo "</div>";
-	?>
+	<?php
+
+		echo "<div class='proname narrowcurve'>";
+		echo "<div class='prourl'><a href='";
+		echo $project->getProjectURL();
+		echo "' >";
+		echo strtoupper($project->getProjectName());
+		echo "</a></div>";
+		echo "<div class='prolang'>";
+		echo $project->getProjectLanguage();
+		echo "</div>";
+		echo "</div>";
+		echo "<div class='prodesc'>";
+		echo $project->getProjectDescription();
+		echo "</div>";
+		echo "<div class='procont'>";
+		echo "<div class='createD'>";
+		echo $project->getProjectCreatedDate();
+		echo "</div>";
+		echo "<div class='homeURL'>";
+		echo "<a href='";
+		echo $project->getProjectHomeURL();
+		echo "'  class='button white'>Home URL</a>";
+		echo "</div>";
+		echo "</div>";
+		echo "<div class='procont'>";
+		echo "<div class='postD'>";
+		echo $project->getProjectPushDate();
+		echo "</div>";
+		echo "<div class='URL '>";
+		echo "<a href='";
+		echo $project->getProjectURL();
+		echo "' class='button white' >URL</a>";
+		echo "</div>";
+		echo "</div>";
+?>
 	</div> <!-- projects -->
 	<?php
+
 	}
-	
+
 }
 ?></div> <!--myprojects--><?php
+
 ?>
 
 </div> <!-- projectList -->
